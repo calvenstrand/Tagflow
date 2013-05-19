9f570b59c0f114cfe2e2184f0e338422157b7aeb
Rasmus Lager <rasmus.lager@chas.se>
2013-05-19 17:41:47 +0200

    added the first working version of the scripts with instagram and twitterfeeds

---
+++ js/rasmus.js
@@ -0,0 +1,82 @@
+//Function to get more than one tag out of the instagetter
+//When calling the function the params should be in a string like - instaGetter('firsttag, secondtag, thirdtag');
+//The function will split on the commas and create a feed for each tag
+function instaGetter(params){
+
+	array_of_tags = params.split(', ');
+	var name_of_tag;
+	var currentOBJ = '';
+	var doFeed = true;
+	
+
+	for (var i = 0; i < array_of_tags.length; i++){
+		//The tagname to use
+		if(currentOBJ == ''){
+		name_of_tag = array_of_tags[i];
+		$('#instafeed').append('<div id="'+name_of_tag+'"></div>');
+		}
+		
+		var feed = new Instafeed({
+			
+			target: name_of_tag,
+			get: 'tagged',
+			tagName: name_of_tag,
+			clientId: 'bf5093520bb54d3cac22e936e98a3804',
+			limit: 60,
+			template: '<div class="instaPost"><div class="instaPicture"><a href="{{link}}"><img src="{{image}}" alt="instaimg"></a></div><p class="instaUser"><a href="{{link}}">{{model.user.username}}</a></p></div>',
+			success: function(data){
+			
+			///////7
+			if (currentOBJ == ''){
+				doFeed = true;
+				currentOBJ = data;
+				console.log('it changed');
+			}else{
+			if(data.data[0].id == currentOBJ.data[0].id){
+				//Do nothing
+				console.log('exactly the same');
+				doFeed = false;
+
+			}else{
+				doFeed = true;
+				currentOBJ = data;
+				console.log('it changed');
+			}
+			}
+			/////////
+
+
+
+			}
+
+		});
+
+feed.run();
+		setInterval(function(){
+	//Run the feed
+	if(doFeed){
+		feed.mock = true;
+		feed.run();
+		console.log('should be imgs')
+	}else{
+		feed.mock = false;
+		feed.run();
+		console.log('should not put imgs');
+	}
+		
+	
+	},10000)
+
+}
+
+
+
+
+
+
+
+	
+}
+
+
+instaGetter('lovemoore');

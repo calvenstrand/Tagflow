58551aca792348cfc1905a7c55a234e2713bf8f5
Rasmus Lager <rasmus@lagerdev.se>
2013-06-27 13:41:35 +0200

    a little codechanges

--- js/application.js
+++ js/application.js
@@ -12,13 +12,43 @@ var leIntervalStarted = false;
 var currentCollection;
 var MODE;
 
+//Use the config to set important values
 
 Moustagram.config = {
   clientId: "bf5093520bb54d3cac22e936e98a3804",
   feedUrl: "https://api.instagram.com/v1/users/self/media/recent?access_token=",
-  tag: 'batman',
+  tag: 'batmanz',
+  mode: function(string){
+    if(string === 'tvMode'){
+      this.tvMode();
+    }else if(string === 'infiniteMode'){
+      this.infiniteMode();
+    }
+  },
+  tvMode: function () {
+      MODE = 'tvMode';
+      //Moustagram.config.tag = tag;
+      $("#choosenHash").html('#' + Moustagram.config.tag);
+      var photos = new Moustagram.collections.Photos();
+      photos.url = 'https://api.instagram.com/v1/tags/' + Moustagram.config.tag + '/media/recent/?client_id=' + Moustagram.config.clientId + '&count=23';
+      var view = new Moustagram.views.PhotosView({
+        collection: photos
+      });
+      $("#moustagram").html(view.el);
+    },
+    infiniteMode: function () {
+      MODE = 'infiniteView';
+      //Moustagram.config.tag = tag;
+      $("#choosenHash").html('#' + Moustagram.config.tag);
+      var photos = new Moustagram.collections.Photos();
+      photos.url = 'https://api.instagram.com/v1/tags/' + Moustagram.config.tag + '/media/recent/?client_id=' + Moustagram.config.clientId + '&count=23';
+      var view = new Moustagram.views.InfiniteView({
+        collection: photos
+      });
+      $("#moustagram").html(view.el);
+    
 
-  getRedirectUrl: function () {
+},getRedirectUrl: function () {
     return this.getRootUrl() + '/Hashtag';
   },
 
@@ -28,7 +58,8 @@ Moustagram.config = {
 }
 
 
-Moustagram.storage = {
+
+/*Moustagram.storage = {
 
   getAccessToken: function () {
     return localStorage.getItem('accessToken'); // null if doesn't exist
@@ -41,9 +72,9 @@ Moustagram.storage = {
   destroyAccessToken: function (token) {
     localStorage.clear();
   }
-}
+}*/
 
-
+//photomodel
 Moustagram.models.Photo = Backbone.Model.extend({
   initialize: function () {
     //console.log('Photo initialized');
@@ -79,38 +110,31 @@ Moustagram.views.PhotoView = Backbone.View.extend({
   },
 
   render: function () {
-     var le = this;
-     var img;
-      
-    
+    var le = this;
+    var img;
     username = this.model.get('user').username;
     link = this.model.get('link');
-    
+
     this.$el.append('<div style="display:hidden;" class="instaPost"></div>');
     this.$el.children('.instaPost').append('<span id="instaUser-container"><p class="instaUser"><a href="' + link + '">' + username + '</a></p></span>');
     this.$el.children('.instaPost').append('<a class="imgLink" href="' + link + '"></a>');
     img = new Image();
-   
 
-    
     this.$el.find('.instaPost').waitForImages(function() {
     // All descendant images have loaded, now slide up.
-    $(img).load( function(){ 
-                    //once image is loaded add it to the a and remove the preloading 
-                   le.$el.find('.imgLink').append( img ); 
-                    // do other stuff 
-                     
-                    le.$el.show();
 
-                }); 
-   
+      $(img).load( function(){ 
+        //once image is loaded add it to the a and remove the preloading 
+        le.$el.find('.imgLink').append( img ); 
 
-    $(this).fadeIn();
-    
-    
-    
+        le.$el.show();
+
+      }); 
+
+      $(this).fadeIn();
       
-});
+    });
+    //Set src for img which  triggers the load
     img.src = this.model.get('images').low_resolution.url;
        
 
@@ -134,17 +158,17 @@ Moustagram.views.PhotosView = Backbone.View.extend({
       add: true,
       dataType: 'jsonp',
       success: function (collection, response) {
-        console.log('completed fetch');
+        //console.log('completed fetch');
       },
       error: function (collection, response) {
-        console.log('fetch fucked up');
+        //console.log('fetch fucked up');
       }
     });
 
     
       setInterval(function () {
         leIntervalStarted = true;
-        console.log('starting fetch');
+        //console.log('starting fetch');
         currentCollection.fetch({
           remove: false,
           limit: 40,
@@ -163,19 +187,6 @@ Moustagram.views.PhotosView = Backbone.View.extend({
 
   },
 
-  //Not used atm render single instead of just render
-  render: function () {
-    console.log('PhotosView rendering');
-    $(this.el).append(_.template($(this.template).html(), {}));
-    view = this;
-    _.each(currentCollection.models, function (photo) {
-      var photoView = new Moustagram.views.PhotoView({
-        model: photo
-      });
-      view.$el.append(photoView.render().el);
-    });
-    return this;
-  },
   renderSingle: function (photo) {
     view = this;
 
@@ -199,11 +210,8 @@ Moustagram.views.InfiniteView = Backbone.View.extend({
   template: "#moustagram",
 
   initialize: function () {
-    console.log('INITING??');
-
-    console.log('PhotosView initialized');
+    //console.log('PhotosView initialized');
     var view = this;
-
     currentCollection = view.collection;
     currentCollection.bind('add', this.renderSingle, this);
 
@@ -213,36 +221,30 @@ Moustagram.views.InfiniteView = Backbone.View.extend({
       success: function (collection, response) {
 
         console.log('completed fetch');
-        //view.render();
+        //Set the first scroll
         $(window).scroll(function() {
-  // Modify to adjust trigger point. You may want to add content
-  // a little before the end of the page is reached. You may also want
-  // to make sure you can't retrigger the end of page condition while
-  // content is still loading.
-  if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-    console.log('lescroll');
-    view.gettit(response);
-  }
-});
+          // Modify to adjust trigger point. You may want to add content
+          // a little before the end of the page is reached. You may also want
+          // to make sure you can't retrigger the end of page condition while
+          // content is still loading.
+          if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+            console.log('lescroll');
+            view.gettit(response);
+          }
+        });
         
-
       },
       error: function (collection, response) {
         console.log('fetch fucked up');
       }
     });
 
-
-
   },
+  //get the older photos
   gettit: function (response) {
     var view = this;
-
     var next_page;
-
-
     mj(response.pagination.next_max_id, response.pagination.next_url);
-
     function mj(max_id, nec) {
 
       if (typeof max_id === 'string' && max_id.trim() !== '') {
@@ -252,27 +254,20 @@ Moustagram.views.InfiniteView = Backbone.View.extend({
       return next_page || url;
     }
 
-
-
     currentCollection.fetch({
       url: next_page,
       dataType: 'jsonp',
       success: function (collection, response) {
         console.log('completed fetch');
         var resp = response;
+        //Unbind the scroll and then set another
         $(window).unbind('scroll');
-       
-        $(window).scroll(function() {
-  // Modify to adjust trigger point. You may want to add content
-  // a little before the end of the page is reached. You may also want
-  // to make sure you can't retrigger the end of page condition while
-  // content is still loading.
-  if ($(window).scrollTop() == $(document).height() - $(window).height()) {
-    console.log('lescroll');
-    view.gettit(response);
-  }
-});
-        //view.render();
+        $(window).scroll(function() { 
+          if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+            console.log('lescroll');
+            view.gettit(response);
+          }
+        });
       },
       error: function (collection, response) {
         console.log('fetch fucked up');
@@ -315,40 +310,15 @@ Moustagram.views.ErrorView = Backbone.View.extend({
 
 
 jQuery(function ($) {
-
-
-  var Moustagrammer = {
-
-    initialize: function () {
-      console.log('tja');
-    },
-    //Run the script and put in which tag to show photos from.
-    tvMode: function (tag) {
-      MODE = 'tvMode';
-      Moustagram.config.tag = tag;
-      $("#choosenHash").html('#' + Moustagram.config.tag);
-      var photos = new Moustagram.collections.Photos();
-      photos.url = 'https://api.instagram.com/v1/tags/' + Moustagram.config.tag + '/media/recent/?client_id=' + Moustagram.config.clientId + '&count=23';
-      var view = new Moustagram.views.PhotosView({
-        collection: photos
-      });
-      $("#moustagram").html(view.el);
-    },
-    infiniteMode: function (tag) {
-      MODE = 'infiniteView';
-      Moustagram.config.tag = tag;
-      $("#choosenHash").html('#' + Moustagram.config.tag);
-      var photos = new Moustagram.collections.Photos();
-      photos.url = 'https://api.instagram.com/v1/tags/' + Moustagram.config.tag + '/media/recent/?client_id=' + Moustagram.config.clientId + '&count=23';
-      var view = new Moustagram.views.InfiniteView({
-        collection: photos
-      });
-      $("#moustagram").html(view.el);
-    }
-
-
-  };
+ 
   //Run this motha
-  Moustagrammer.infiniteMode('batman');
+var loda = Moustagram;
+loda.config.tag='batman';
+loda.config.mode('tvMode')
+  
+}
 
-});
+
+ 
+
+);
